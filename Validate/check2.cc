0ed57921c04b5e81b6bee5b9875145c04eb2825e
bajarang <bajarang.sutar@cern.ch>
2017-11-17 11:37:03 +0530

    More thorough check

---
+++ Validate/check2.cc
@@ -0,0 +1,30 @@
+/*
+ * This code performs three checks
+ * 1. Finds out corrupted files (easy check)
+ * 2. Finds out empty plots     (easy check)
+ * 3. Adds all the events and compares with total (logic needed to add certain set of files)
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "TFile.h"
+#include "TH1D.h"
+using namespace std;
+
+int main(){
+
+  // 1. FIND OUT CORRUPTED FILES (EACH CHECK) 
+  ////////////Open root files////////////////
+  
+
+  
+  // 2. FINDS OUT EMPTY PLOTS     (EASY CHECK)
+  ////////////Open plots of interest////////////////
+ 
+ 
+  // 3. ADDS ALL THE EVENTS AND COMPARES WITH TOTAL (LOGIC NEEDED TO ADD CERTAIN SET OF FILES)
+  ////////////Explain logic here////////////////
+
+  return 0;
+}
