5c019590794b12451427e82932eb0cedaddc7fd1
bajarang <bajarang.sutar@cern.ch>
2017-11-27 12:21:10 +0530

    Add one more parameter indicating range

--- Validate/generateFileNames.cc
+++ Validate/generateFileNames.cc
@@ -1,42 +1,43 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include "generateFileNames.h"
 using namespace std;
 
-string generateFileNames(string charge, string sample, string syst, string direction, string doQCD){
+string generateFileNames(string charge, string sample, string doQCD, string syst, string direction, string appendRange){
 
   //////////////////////////////
   string leptonFlavor = "SMu_";
-  string energy = "_8TeV_";  
+  string energy = "8TeV_";  
   
   //////////////////////////////
   string effCorr;
   string trigCorr;
   if(sample.find("Data")!=string::npos){
-    effCorr = "_EffiCorr_0_";
+    effCorr = "EffiCorr_0_";
     trigCorr = "TrigCorr_1_";
   }
   else {
-    effCorr = "_EffiCorr_1_";
+    effCorr = "EffiCorr_1_";
     trigCorr = "TrigCorr_1_";
   }
 
   //////////////////////////////
   string strDirection;
   if(direction=="0"){
-    strDirection = "CN";
+    strDirection = "CN_";
   }
   else if (direction=="1"){
-    strDirection = "UP";
+    strDirection = "UP_";
   }
   else {
-    strDirection = "DN";
+    strDirection = "DN_";
   }
 
   //////////////////////////////
   /// eg. SMu_8TeV_Data_dR_5311_00001_WP_EffiCorr_0_TrigCorr_1_Syst_0_CN_JetPtMin_30_VarWidth_BVeto_QCD0_MET15_mT50.root
   string fullFileName;
-  fullFileName = leptonFlavor + energy + sample + charge + effCorr + trigCorr + "Syst_" + syst + strDirection + "_JetPtMin_30_VarWidth_BVeto_QCD" + doQCD + "_MET15_mT50.root";
+  fullFileName = leptonFlavor + energy + sample + "_dR_5311_" + appendRange  + charge + "_" + effCorr + trigCorr + "Syst_" + syst + "_" + strDirection + "JetPtMin_30_VarWidth_BVeto_QCD" + doQCD + "_MET15_mT50.root";
   cout << "Returning string : " << fullFileName << endl;
   return fullFileName;
 }
--- Validate/generateFileNames.h
+++ Validate/generateFileNames.h
@@ -1,6 +1,9 @@
 #ifndef _generateFileNames_h_
 #define _generateFileNames_h_
+#include <iostream>
+#include <string>
+using namespace std;
 
-string generateFileNames(string charge, string sample, string syst, string direction, string doQCD); // 5 arguments 
+string generateFileNames(string charge, string sample, string doQCD, string syst, string direction, string appendRange); // 6 arguments 
 
 #endif
