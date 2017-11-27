cc13ac5b21125b7ba0ef80f37e0db8ab3fe6accc
bajarang <bajarang.sutar@cern.ch>
2017-11-27 23:08:01 +0530

    Line up merge jobs

---
+++ Validate/lineUpMergeJobs.sh
@@ -0,0 +1,10 @@
+declare -a Sample=('Data' 'TTJets' 'ZZ' 'WZ' 'WW' 'T_s_channel' 'T_t_channel' 'T_tW_channel' 'Tbar_s_channel' 'Tbar_t_channel' 'Tbar_tW_channel' 'DYJets10to50' 'DYJets_MIX_UNFOLDING' 'WJetsALL_MIX_UNFOLDING')
+for jj in {0..13}
+#for jj in {0..12}
+do
+  for ii in {0..3}
+  do
+    ./mergeHistos  1 ${Sample[jj]} $ii 0 0
+    ./mergeHistos -1 ${Sample[jj]} $ii 0 0
+  done
+done
