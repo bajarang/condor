1470cd79a549735bcf3dc168903a8c64f59e97e9
bajarang <bajarang.sutar@cern.ch>
2017-12-05 11:59:19 +0530

    Add comment

--- Validate/lineUpMergeJobs.sh
+++ Validate/lineUpMergeJobs.sh
@@ -1,6 +1,9 @@
+###################################
+# COMPILE mergeHistos.cc FIRST #
+###################################
+
 declare -a Sample=('Data' 'TTJets' 'ZZ' 'WZ' 'WW' 'T_s_channel' 'T_t_channel' 'T_tW_channel' 'Tbar_s_channel' 'Tbar_t_channel' 'Tbar_tW_channel' 'DYJets10to50' 'DYJets_MIX_UNFOLDING' 'WJetsALL_MIX_UNFOLDING')
 for jj in {0..13}
-#for jj in {0..12}
 do
   for ii in {0..3}
   do
