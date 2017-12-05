cd597df734a11c4e5aa7ecc2bebd3761bee51e17
bajarang <bajarang.sutar@cern.ch>
2017-12-05 10:44:37 +0530

    Add comment

--- lineUpCondorJobs.sh
+++ lineUpCondorJobs.sh
@@ -1,12 +1,16 @@
-for ii in {1..579}   #data
+#for ii in {1..579}   #data
 #for ii in {580..689} #BkgdMC (including DYJets_MIX_UNFOLDING)
 #for ii in {690..928} #signalMC
 #for ii in {605..689}  #Only DYJets_MIX_UNFOLDING
+for ii in {1..102}   #data
 do
   for jj in {0..3}
   do
-    ./runFinalCondor.sh  1 $ii $jj 2  
-    ./runFinalCondor.sh -1 $ii $jj 2 
+    # ./runFinalCondor.sh WCHARGE LINENUM DOQCD SYSTEMATICS DIRECTION 
+    ./runFinalCondor.sh  1 $ii $jj 1 -1  
+    ./runFinalCondor.sh  1 $ii $jj 1  1  
+    ./runFinalCondor.sh -1 $ii $jj 1 -1
+    ./runFinalCondor.sh -1 $ii $jj 1  1
   done
 done
 #./runFinalCondor.sh 2
