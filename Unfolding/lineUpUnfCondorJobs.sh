8969ebcd889ca159e36d2124b3e9e47231f67787
bajarang <bajarang.sutar@cern.ch>
2017-12-08 12:02:47 +0530

    Add more jobs

--- Unfolding/lineUpUnfCondorJobs.sh
+++ Unfolding/lineUpUnfCondorJobs.sh
@@ -7,17 +7,150 @@
 # SYSTEMATICS : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
 # DIRECTION   : 0 or 1 or -1
 
-#wplus
+###--- wplus ---###
+##-- Inc --##
+#- 0jet -#
 source runUnfFinalCondor.sh   1 lepEta_Zinc0jet 0 0  0 25_above
 source runUnfFinalCondor.sh   1 lepEta_Zinc0jet 0 0  0 25_30
 source runUnfFinalCondor.sh   1 lepEta_Zinc0jet 0 0  0 30_35
 source runUnfFinalCondor.sh   1 lepEta_Zinc0jet 0 0  0 35_40
 source runUnfFinalCondor.sh   1 lepEta_Zinc0jet 0 0  0 40_45
 source runUnfFinalCondor.sh   1 lepEta_Zinc0jet 0 0  0 45_above
-#wminus
+#- 1jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zinc1jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zinc1jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zinc1jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zinc1jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zinc1jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zinc1jet 0 0  0 45_above
+#- 2jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zinc2jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zinc2jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zinc2jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zinc2jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zinc2jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zinc2jet 0 0  0 45_above
+#- 3jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zinc3jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zinc3jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zinc3jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zinc3jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zinc3jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zinc3jet 0 0  0 45_above
+#- 4jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zinc4jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zinc4jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zinc4jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zinc4jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zinc4jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zinc4jet 0 0  0 45_above
+##-- Exc --##
+#- 0jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zexc0jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zexc0jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zexc0jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zexc0jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zexc0jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zexc0jet 0 0  0 45_above
+#- 1jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zexc1jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zexc1jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zexc1jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zexc1jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zexc1jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zexc1jet 0 0  0 45_above
+#- 2jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zexc2jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zexc2jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zexc2jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zexc2jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zexc2jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zexc2jet 0 0  0 45_above
+#- 3jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zexc3jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zexc3jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zexc3jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zexc3jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zexc3jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zexc3jet 0 0  0 45_above
+#- 4jet -#
+source runUnfFinalCondor.sh   1 lepEta_Zexc4jet 0 0  0 25_above
+source runUnfFinalCondor.sh   1 lepEta_Zexc4jet 0 0  0 25_30
+source runUnfFinalCondor.sh   1 lepEta_Zexc4jet 0 0  0 30_35
+source runUnfFinalCondor.sh   1 lepEta_Zexc4jet 0 0  0 35_40
+source runUnfFinalCondor.sh   1 lepEta_Zexc4jet 0 0  0 40_45
+source runUnfFinalCondor.sh   1 lepEta_Zexc4jet 0 0  0 45_above
+
+###--- wminus ---###
+##-- Inc --##
+#- 0jet -#
 source runUnfFinalCondor.sh  -1 lepEta_Zinc0jet 0 0  0 25_above
 source runUnfFinalCondor.sh  -1 lepEta_Zinc0jet 0 0  0 25_30
 source runUnfFinalCondor.sh  -1 lepEta_Zinc0jet 0 0  0 30_35
 source runUnfFinalCondor.sh  -1 lepEta_Zinc0jet 0 0  0 35_40
 source runUnfFinalCondor.sh  -1 lepEta_Zinc0jet 0 0  0 40_45
 source runUnfFinalCondor.sh  -1 lepEta_Zinc0jet 0 0  0 45_above
+#- 1jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zinc1jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zinc1jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zinc1jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zinc1jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zinc1jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zinc1jet 0 0  0 45_above
+#- 2jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zinc2jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zinc2jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zinc2jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zinc2jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zinc2jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zinc2jet 0 0  0 45_above
+#- 3jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zinc3jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zinc3jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zinc3jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zinc3jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zinc3jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zinc3jet 0 0  0 45_above
+#- 4jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zinc4jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zinc4jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zinc4jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zinc4jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zinc4jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zinc4jet 0 0  0 45_above
+##-- Exc --##
+#- 0jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zexc0jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zexc0jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zexc0jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zexc0jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zexc0jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zexc0jet 0 0  0 45_above
+#- 1jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zexc1jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zexc1jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zexc1jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zexc1jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zexc1jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zexc1jet 0 0  0 45_above
+#- 2jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zexc2jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zexc2jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zexc2jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zexc2jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zexc2jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zexc2jet 0 0  0 45_above
+#- 3jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zexc3jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zexc3jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zexc3jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zexc3jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zexc3jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zexc3jet 0 0  0 45_above
+#- 4jet -#
+source runUnfFinalCondor.sh  -1 lepEta_Zexc4jet 0 0  0 25_above
+source runUnfFinalCondor.sh  -1 lepEta_Zexc4jet 0 0  0 25_30
+source runUnfFinalCondor.sh  -1 lepEta_Zexc4jet 0 0  0 30_35
+source runUnfFinalCondor.sh  -1 lepEta_Zexc4jet 0 0  0 35_40
+source runUnfFinalCondor.sh  -1 lepEta_Zexc4jet 0 0  0 40_45
+source runUnfFinalCondor.sh  -1 lepEta_Zexc4jet 0 0  0 45_above
