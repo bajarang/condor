007fc32c576861e8a545f7d9ede93889224bdc5a
bajarang <bajarang.sutar@cern.ch>
2017-12-08 12:03:46 +0530

    Choose a particular sample to merge

--- Validate/lineUpMergeJobs.sh
+++ Validate/lineUpMergeJobs.sh
@@ -3,11 +3,12 @@
 ###################################
 
 declare -a Sample=('Data' 'TTJets' 'ZZ' 'WZ' 'WW' 'T_s_channel' 'T_t_channel' 'T_tW_channel' 'Tbar_s_channel' 'Tbar_t_channel' 'Tbar_tW_channel' 'DYJets10to50' 'DYJets_MIX_UNFOLDING' 'WJetsALL_MIX_UNFOLDING')
-for jj in {0..13}
-do
+jj=12
+#for jj in {0..13}
+#do
   for ii in {0..3}
   do
     ./mergeHistos  1 ${Sample[jj]} $ii 0 0
     ./mergeHistos -1 ${Sample[jj]} $ii 0 0
   done
-done
+#done
