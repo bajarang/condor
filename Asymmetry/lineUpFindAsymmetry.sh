1ca145f10b3c81aadf7f3d1976107e3e6246d1fc
bajarang <bajarang.sutar@cern.ch>
2017-12-12 11:32:33 +0530

    Line up multiple asymmetry jobs in one go

---
+++ Asymmetry/lineUpFindAsymmetry.sh
@@ -0,0 +1,6 @@
+./findAsymmetry UNF lepEta_Zinc0jet 25_above 0 0 0
+./findAsymmetry UNF lepEta_Zinc0jet 25_30    0 0 0
+./findAsymmetry UNF lepEta_Zinc0jet 30_35    0 0 0
+./findAsymmetry UNF lepEta_Zinc0jet 35_40    0 0 0
+./findAsymmetry UNF lepEta_Zinc0jet 40_45    0 0 0
+./findAsymmetry UNF lepEta_Zinc0jet 45_above 0 0 0
