c9ed0b6b8c46eab0246930a4ba3a77e013030769
bajarang <bajarang.sutar@cern.ch>
2017-12-12 11:30:42 +0530

    More syst options

--- Validate/lineUpMergeJobs.sh
+++ Validate/lineUpMergeJobs.sh
@@ -3,12 +3,29 @@
 ###################################
 
 declare -a Sample=('Data' 'TTJets' 'ZZ' 'WZ' 'WW' 'T_s_channel' 'T_t_channel' 'T_tW_channel' 'Tbar_s_channel' 'Tbar_t_channel' 'Tbar_tW_channel' 'DYJets10to50' 'DYJets_MIX_UNFOLDING' 'WJetsALL_MIX_UNFOLDING')
-jj=12
 #for jj in {0..13}
-#do
+for jj in {1..12}
+#jj=0
+do
   for ii in {0..3}
   do
-    ./mergeHistos  1 ${Sample[jj]} $ii 0 0
-    ./mergeHistos -1 ${Sample[jj]} $ii 0 0
+    # 0
+    #./mergeHistos  1 ${Sample[jj]} $ii 0 0
+    #./mergeHistos -1 ${Sample[jj]} $ii 0 0
+    # 1
+    #./mergeHistos  1 ${Sample[jj]} $ii 1  1
+    #./mergeHistos  1 ${Sample[jj]} $ii 1 -1
+    #./mergeHistos -1 ${Sample[jj]} $ii 1  1
+    #./mergeHistos -1 ${Sample[jj]} $ii 1 -1
+    # 2
+    #./mergeHistos  1 ${Sample[jj]} $ii 2  1
+    #./mergeHistos  1 ${Sample[jj]} $ii 2 -1
+    #./mergeHistos -1 ${Sample[jj]} $ii 2  1
+    #./mergeHistos -1 ${Sample[jj]} $ii 2 -1
+    # 3
+    ./mergeHistos  1 ${Sample[jj]} $ii 3  1
+    ./mergeHistos  1 ${Sample[jj]} $ii 3 -1
+    ./mergeHistos -1 ${Sample[jj]} $ii 3  1
+    ./mergeHistos -1 ${Sample[jj]} $ii 3 -1
   done
-#done
+done
